4fdc6fc7df67dae4e3a127968402870f3144e0bd
wangnewbie <wangnewbie@163.com>
2019-07-09 16:13:32 +0800

    配置Rem适配

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import 'amfe-flexible'
 import { Button, Tabbar, TabbarItem } from 'vant'
 
 Vue.use(Button).use(Tabbar).use(TabbarItem)
